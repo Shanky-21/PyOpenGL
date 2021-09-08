67957aefee6da1a3fd16ca55cea36f5f0b009fbc
Shashank <shashankdwivedi9648@gmail.com>
2021-09-08 20:43:33 +0530

    glClearColor

--- Creating a Window New OpenGl.py
+++ Creating a Window New OpenGl.py
@@ -1,15 +1,29 @@
+
+
+"""
+Creating a Simple Window - New OpenGL
+author - @Pycasm
+"""
+
 import glfw
 from OpenGL.GL import*
 
 def framebuffer_size_callback(window, width, height):
     glViewport(0,0, width, height)
 
+def processInput(window):
+    if glfw.get_key(window,glfw.KEY_ESCAPE) == glfw.PRESS:
+        glfw.set_window_should_close(window,True)
+
+
 if not glfw.init():
     print('Can\'t initialize glfw')
     exit()
 
 
-window = glfw.create_window(1000,800,"Main Window", None, None)
+
+
+window = glfw.create_window(1000,800,"Pycasm Window", None, None)
 if not window:
     glfw.terminate()
     print("Glfw window can't be created")
@@ -20,8 +34,15 @@ glfw.make_context_current(window)
 glfw.set_framebuffer_size_callback(window,framebuffer_size_callback)
 
 while not glfw.window_should_close(window):
-    glfw.swap_buffers(window)
+    #Input
+    processInput(window)
+    #Rendering Commands Here
+    glClearColor(0.2,0.3,0.3,1.0)
+    glClear(GL_COLOR_BUFFER_BIT)
+    #Check and Call events and swap the buffers
     glfw.poll_events()
+    glfw.swap_buffers(window)
+    
 
 glfw.terminate()
 
