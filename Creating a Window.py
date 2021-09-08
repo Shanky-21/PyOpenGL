8259b084db8d0f95881ca07eded99a9440a435c5
Shashank <shashankdwivedi9648@gmail.com>
2021-09-08 19:20:26 +0530

    Update

---
+++ Creating a Window.py
@@ -0,0 +1,24 @@
+"""
+OpenGL in Python -  (Triangle Program)
+author - Shashank Dwivedi
+"""
+
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+
+width , height = 500, 400
+
+def draw():
+    #glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+
+    glutSwapBuffers()
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA )
+glutInitWindowSize(width, height)
+glutInitWindowPosition(200, 200)
+window = glutCreateWindow("Opengl Window In Python")
+glutDisplayFunc(draw)
+glutIdleFunc(draw)
+glutMainLoop()
